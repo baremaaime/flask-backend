a9990ec2da16bc96716b8d9d483d8dd5036436ca
unknown <baremaaime@gmail.com>
2025-07-17 23:47:09 +0200

    Deploying Backend

---
+++ flask_api.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+import pickle
+import numpy as np
+import os
+
+app = Flask(__name__)
+model = pickle.load(open("model.pkl", "rb"))
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.json
+    features = np.array(data["features"]).reshape(1, -1)
+    prediction = model.predict(features)[0]
+    return jsonify({"prediction": int(prediction)})
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
